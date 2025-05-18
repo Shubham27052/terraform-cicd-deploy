ad9f0b784edd8723971dd9cf25b2dc80401b12d6
Shubham Agarwal <shubham.27052@gmail.com>
2025-05-18 19:00:16 +0530

    update_sp

--- provider.tf
+++ provider.tf
@@ -9,10 +9,10 @@ terraform {
 
   backend "azurerm" {
     use_azuread_auth     = true                                   # Can also be set via `ARM_USE_AZUREAD` environment variable.
-    tenant_id            = "62add8d4-1f2a-4fed-8f52-1071990464a1" # Can also be set via `ARM_TENANT_ID` environment variable.
-    subscription_id = "d76c02a2-6f94-4c24-be34-8147ea509940"
-    resource_group_name = "dev-sftp"
-    storage_account_name = "tfbackendshubham"  # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    tenant_id            = "980a9497-3393-4e89-9671-4f0f938006fe" # Can also be set via `ARM_TENANT_ID` environment variable.
+    subscription_id      = "f5a72aa1-60c1-4225-90ba-4e5273d6af91"
+    resource_group_name  = "tfstaterg"
+    storage_account_name = "tfstateshubham"    # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"           # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
@@ -22,8 +22,8 @@ terraform {
 provider "azurerm" {
 
   features {}
-  tenant_id       = "62add8d4-1f2a-4fed-8f52-1071990464a1"
-  subscription_id = "d76c02a2-6f94-4c24-be34-8147ea509940"
+  tenant_id       = "980a9497-3393-4e89-9671-4f0f938006fe"
+  subscription_id = "f5a72aa1-60c1-4225-90ba-4e5273d6af91"
 
 
 }
